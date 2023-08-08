c32256d38e6a410cb6fa12651f58854a4fb6da81
mmbcode <141781573+mmbcode@users.noreply.github.com>
2023-08-08 17:37:10 -0400

    Update batting.py
    updated spacing.

--- batting.py
+++ batting.py
@@ -1,13 +1,13 @@
 # Collect number of hits(h),at bats(ab),validate data,calculate batting average(ba),display
 
-check=0
+check = 0 
 
 while check < 1:
-	h=input("Enter the player's number of hits: ")
-	ab=input("Enter the player's times at bat: ")
+	h = input("Enter the player's number of hits: ")
+	ab = input("Enter the player's times at bat: ")
 
 	if (h.isdigit()) and (ab.isdigit())and (int(ab) >= int(h)):
-		ba="{:.3f}".format(int(h)/int(ab))
+		ba = "{:.3f}".format(int(h)/int(ab))
 		print("\n\n\tA player with",h,"hits and",ab,"at bats would have a batting average of",ba,"\n\n\n")
 		check=1
 	else:	
